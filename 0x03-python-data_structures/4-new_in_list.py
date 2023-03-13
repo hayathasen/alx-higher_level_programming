022cb29dd12b76784bf4f8b836c13bad9b624063
hayathasen <mahabubahasen31@gmail.com>
2023-03-13 08:05:06 -0700

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    hay = my_list[:]
+    dup = my_list[:]
     if idx >= 0 and idx < len(hay):
-        hay[idx] = element
-    return hay
+        dup[idx] = element
+    return dup
