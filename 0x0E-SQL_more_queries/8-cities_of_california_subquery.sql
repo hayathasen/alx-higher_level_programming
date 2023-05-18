ff26222f12de59baa15dde27585d4a556fd41b5a
hayathasen <mahabubahasen31@gmail.com>
2023-05-18 13:35:58 -0700

    tasks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
--- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
--- The database name will be passed as an argument of the mysql command
-
-SELECT COUNT(*) FROM first_table WHERE id=89;
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
