0600febd0f8980213502653dfe53d0d675b5f08c
hayathasen <mahabubahasen31@gmail.com>
2023-05-18 10:38:57 -0700

    task 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+
+mysql -hlocalhost -uroot -p <database_name> -e "SHOW TABLES;"
