62af7cc7d75d24624529ad40d28939570b1c53ca
hayathasen <mahabubahasen31@gmail.com>
2023-05-18 13:30:59 -0700

    tasks

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,6 @@
--- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
--- The database name will be passed as an argument of the mysql command
-
-SELECT COUNT(*) FROM first_table WHERE id=89;
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all rows of tables in a database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
