f18c2fb76a86e2c92cfa79e48e96be67aa46af7a
hayathasen <mahabubahasen31@gmail.com>
2023-06-18 13:06:41 -0700

    0x0F-python-object_relational_mapping# git push

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all states via SQLAlchemy"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.contains('a')):
+        print('{0}: {1}'.format(instance.id, instance.name))
