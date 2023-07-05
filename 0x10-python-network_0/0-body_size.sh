dd7eb00e3a93fd0841f5fe1094ee37c9a1e993b3
hayathasen <mahabubahasen31@gmail.com>
2023-07-05 11:57:55 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-url=$1
-
-response=$(curl -s -w "%{size_download}" $url)
-
-echo "The size of the body of the response is $response bytes."
-
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
